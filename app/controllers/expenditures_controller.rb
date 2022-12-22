096eb06b455fdca606ce8d67152b5ec6560f4684
Profsain <profsaincrown@gmail.com>
2022-12-22 15:47:13 +0000

    add backlink

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -8,9 +8,15 @@ class ExpendituresController < ApplicationController
     @categories.each do |category|
       @expenditure = Expenditure.new(name: params[:expenditure][:name], amount: params[:expenditure][:amount],
                                      category_id: category.to_i, user_id: current_user.id)
-      @expenditure.save
+      if @expenditure.save
+        flash[:notice] = 'Expenditure created successfully'
+        redirect_to category_path(category.to_i)
+      else
+        flash[:alert] = 'Expenditure not created'
+        render :new
+      end
     end
-    redirect_to expenditures_path
+    
   end
 
   private
